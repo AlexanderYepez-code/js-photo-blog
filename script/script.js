4606b05e1ff2c8f8b238d4e33f3a00674159e30e
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-11-18 19:14:11 +0100

    feat: effetti inseriti con hover manca posizionare la puntina nel centro

--- script/script.js
+++ script/script.js
@@ -1,20 +1,30 @@
 const cardPhoto = document.querySelector(".bachecca");
-const aPI = `https://lanciweb.github.io/demo/api/pictures/`
+const api = "https://lanciweb.github.io/demo/api/pictures/";
 
-axios.get(aPI).then((resp) => {
-    console.log(resp)
-    const photoArray = resp.data;
-    let photoString = " ";
-    photoArray.forEach((curPhoto) =>{
-        photoString += `<div class="card col p-20">
-                    <img src="${curPhoto.url}" alt="">
-                    <h3>${curPhoto.title}</h3>
-                    <p>${curPhoto.date}</p></div> `
+// Loader 
+const loader = `<p>Caricamento...</p>`;
 
-        
-                    
-                
-       
-    });
-    cardPhoto.innerHTML = photoString
-})
+async function loadPhotos() {
+  // Mostra loader
+  cardPhoto.innerHTML = loader;
+
+  // Richiesta API
+  const { data: photoArray } = await axios.get(api);
+
+  // Genera HTML con destrutturazione
+  const photoString = photoArray
+    .map(({ url, date, title }) => `
+      <div class="card col p-20">
+      <img class="puntina" src="./img/pin.svg" alt="">
+        <img src="${url}" alt="${title}">
+        <p class="fontdate">${date}</p>
+        <h3 class="uppercase">${title}</h3>
+      </div>
+    `)
+    .join("");
+
+  // Inserisce le card
+  cardPhoto.innerHTML = photoString;
+}
+
+loadPhotos();
