7d67a27958f78446e7a163b0d373b17f174ee521
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-11-19 17:17:17 +0100

    Feat: esercizio finito

--- script/script.js
+++ script/script.js
@@ -1,52 +1,46 @@
+const API_URL = "https://lanciweb.github.io/demo/api/pictures/";
 const cardPhoto = document.querySelector(".bachecca");
-const aPI = `https://lanciweb.github.io/demo/api/pictures/`
-const overlay = document.querySelector(".overlay")
-let content = document.querySelector(".content")
-const close = document.querySelector(".btn")
+const overlay = document.querySelector(".overlay");
+const content = document.querySelector(".content");
+const close = document.querySelector(".btn");
 
-axios.get(aPI).then((resp) => {
+axios.get(API_URL).then((resp) => {
   console.log(resp)
   const photoArray = resp.data;
-  let photoString = " ";
   photoArray.forEach((curPhoto) => {
-    photoString += `<div class="card col p-20"
-         data-url="${curPhoto.url}"
-         data-date="${curPhoto.date}"
-         data-title="${curPhoto.title}">
+    cardPhoto.innerHTML += 
+    `<div class="card col p-20"
+        data-url="${curPhoto.url}"
+        data-date="${curPhoto.date}"
+        data-title="${curPhoto.title}">
         <img class="puntina" src="./img/pin.svg" alt="">
         <img src="${curPhoto.url}" alt="">
         <p class="fontdate">${curPhoto.date}</p>
         <h3 class="uppercase">${curPhoto.title}</h3>
     </div>
 `;
-
-
-
-
-
-  });
-  cardPhoto.innerHTML = photoString
-
+});
 
   document.querySelectorAll(".card").forEach(card => {
     card.addEventListener("click", () => {
       console.log(card)
       overlay.classList.remove("d-none");
-      content.innerHTML = `<div class="content">
+      content.innerHTML = 
+            `<div class="content">
                 <img src="${card.dataset.url}" alt="">
                 <p class="fontdate">${card.dataset.date}</p>
                 <h3 class="uppercase">${card.dataset.title}</h3>
-                </div>
-            `;
+              </div>`;
 
     });
 
   });
 
 });
-close.addEventListener("click", () => {
+
+close.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+function closeModal() {
   overlay.classList.add("d-none");
-});
-overlay.addEventListener("click", () => {
-  overlay.classList.add("d-none");
-});
+}
